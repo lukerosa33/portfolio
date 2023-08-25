4168dc9f90ed2572e256c4efce8784f3e6bd0e48
Lucas Rosa <lucasrosa@Lucass-MacBook-Pro.local>
2023-08-25 15:01:57 -0400

    image update

--- script.js
+++ script.js
@@ -1,49 +1,39 @@
-function toggleCard(card) {
-    // Close any active popups.
-    closeActivePopup();
+document.addEventListener("DOMContentLoaded", function() {
+    let projectCards = document.querySelectorAll('.project-card');
+    let detailViews = document.querySelectorAll('.details-view');
+    let backArrow = document.querySelector('.back-arrow');
 
-    // Open the clicked card's popup.
-    const popup = card.querySelector('.popup');
-    popup.style.display = "block";
-    setTimeout(() => {
-        popup.style.opacity = "1";
-        popup.style.transform = "scale(1)";
-    }, 10);
-    popup.classList.add('active');
-}
-
-function closeCard() {
-    closeActivePopup();
-}
-
-function closeActivePopup() {
-    const activePopups = document.querySelectorAll('.popup.active');
-    activePopups.forEach(popup => {
-        popup.style.opacity = "0";
-        popup.style.transform = "scale(0.95)";
-        setTimeout(() => {
-            popup.style.display = "none";
-            popup.classList.remove('active');
-        }, 300);
+    projectCards.forEach(card => {
+        card.addEventListener('click', function() {
+            let project = card.getAttribute('data-project');
+            
+            // Hide other cards
+            projectCards.forEach(c => {
+                if(c !== card) c.style.opacity = "0";
+            });
+            
+            setTimeout(() => {
+                // Show detailed view
+                document.querySelector(`[data-project="${project}"].details-view`).style.display = "block";
+                
+                // Show back arrow
+                backArrow.style.display = "block";
+            }, 500); // This delay matches the CSS transition time
+        });
     });
-}
 
-document.addEventListener('click', function(e) {
-    const isProjectCard = e.target.closest('.project-card') && !e.target.closest('.popup');
+    backArrow.addEventListener('click', function() {
+        // Hide detailed view
+        detailViews.forEach(view => view.style.display = "none");
 
-    if (!e.target.closest('.popup') && !e.target.classList.contains('close-btn') && !isProjectCard) {
-        closeActivePopup();
-    }
+        // Hide back arrow
+        backArrow.style.display = "none";
+        
+        // Show cards again
+        projectCards.forEach(c => c.style.opacity = "1");
+    });
 });
-document.addEventListener('click', function(e) {
-    const isInsidePopup = e.target.closest('.popup');
-    const isCloseBtn = e.target.classList.contains('close-btn');
-    const isProjectCard = e.target.closest('.project-card') && !isInsidePopup;
 
-    if (!isInsidePopup && !isCloseBtn && !isProjectCard) {
-        closeActivePopup();
-    }
-});
 
 
 
